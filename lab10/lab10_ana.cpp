37c38be82d09a80c7944e7de89d50b3ed4daec08
AnaArante01 <AnaJolynn.Arante01@student.csulb.edu>
2025-10-21 20:48:59 -0700

    starter files

---
+++ lab10/lab10_ana.cpp
@@ -0,0 +1,7 @@
+/*
+* This C++ program creates two sets of alphabets. One is regular (7 x 5) and one is bolded (8 x 8).
+* This program allows the user to enter a message and display their message using the created alphabets.
+* CECS 275 - Fall 2025
+* @authors Ana Jolynn Arante, Suphia Sidiqi
+* @version 1.0.0
+*/
