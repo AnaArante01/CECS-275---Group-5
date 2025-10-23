a4efee4ddc5548cac02873c57bdfd3cbc6649685
AnaArante01 <AnaJolynn.Arante01@student.csulb.edu>
2025-10-22 19:41:51 -0700

    main

--- lab10/lab10.cpp
+++ lab10/lab10.cpp
@@ -1,7 +1,85 @@
 /*
-* This C++ program creates two sets of alphabets. One is regular (7 x 5) and one is bolded (8 x 8).
-* This program allows the user to enter a message and display their message using the created alphabets.
+* This C++ program takes a text file and produces another text file that capitalizes the first character
+* of each sentence and capitalizes all lowercase i's if it is by itself.
+* 
+* Example Case 1:
+* Input: "c++ is fun. my friends and i are enjoying this assignment."
+* Output: "C++ is fun. My friends and I are enjoying this assignment."
+* 
+* Example Case 2:
+* Input: Alabama,AL,4447100
+* Output: Alabama  AL  4,447,100
+* 
 * CECS 275 - Fall 2025
 * @authors Ana Jolynn Arante, Suphia Sidiqi
 * @version 1.0.0
 */
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+#include "functions_ana.h"
+#include "helpers_ana.h"
+
+// Function Prototypes from functions_ana.h
+void displayMainMenu();
+void statesPopulation(char** &arrStates, int numStates);
+
+// Function Prototypes from helpers_ana.h
+void print2DArray(char** arr, int rows);
+
+int main(){
+
+    // Define variables
+    int choice;
+
+    // Display Main Menu
+    displayMainMenu();
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    switch(choice)
+    {
+        case 1: // User chooses to create a text file to capitaliza the first character of each sentence and all individual i's
+        {
+
+
+
+        }
+        case 2: // User chooses to input a text file to output to us_states_population_formatted.txt
+        {
+            // Define variable for the name of the text file
+            string textFile;
+            int rows = 51;   // 50 states
+
+            // Prompt user to enter the name of the input file
+            cout << "Enter the name of the text file: ";
+            cin >> textFile;
+            ifstream in(textFile);
+
+            // Define 2D array of all the states, abbreviations, and populations
+            char** states = getAllStates(in, rows);
+
+            // Function to format the state names, abbreviations, and populations with commas
+            statesPopulation(states, rows);
+
+            // Free up memory
+            for (int i = 0; i < 51; i++){
+                delete[] states[i];
+            }
+            delete[] states;
+
+        }
+
+
+
+    }
+
+    return 0;
+}
